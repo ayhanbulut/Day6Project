1ec80170861c5de17cdcf9d3150bc4dd09181f03
Ayhan Bulut <ayhanbulu@gmail.com>
2020-10-14 19:03:46 +0200

    ok added

---
+++ src/test/java/newhello.java
@@ -0,0 +1,12 @@
+public class newhello {
+
+    public static void main(String[] args) {
+        System.out.println("hello again");
+
+        // i made some changes because of practice
+
+        // ok
+        //music is fine
+
+    }
+}
