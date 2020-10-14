eeeffec3c7ff302f07855993329fcaf79253f968
Ayhan Bulut <ayhanbulu@gmail.com>
2020-10-14 19:23:19 +0200

    one line added

--- src/test/java/feature2.java
+++ src/test/java/feature2.java
@@ -2,5 +2,11 @@ public class feature2 {
 
     public static void main(String[] args) {
 
+        System.out.println("hello agin" +
+                "nice");
+
+        System.out.println("good" +
+                "");
+
     }
 }
